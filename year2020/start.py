18b43b9febf2b1963919b46d4dc83d8d8d0086a5
chenguangxu <guangxuchen@oneops.cn>
2020-01-20 17:39:50 +0800

    修改

--- year2020/start.py
+++ year2020/start.py
@@ -4,12 +4,15 @@ from itchat.content import *
 # 微信消息接收
 from new_msg_util import not_friends, clean_not_friends, note_type, backup
 
+not_send = []
+
 
 # 不获取群信息
 @itchat.msg_register([TEXT, PICTURE, FRIENDS, CARD, MAP, SHARING, RECORDING, ATTACHMENT, VIDEO], isFriendChat=True,
                      isGroupChat=False, isMpChat=True)
 def new_year(msg):
     if msg['ToUserName'] == 'filehelper':
+        groupUserName = itchat.search_chatrooms('uuuu')[0]['UserName']
         if '发送人' in msg['Content'][0:3]:
             # 要发送的消息
             # 所有好友
@@ -30,7 +33,9 @@ def new_year(msg):
                     sendMsg = petName + ':\n' + text_br_index(msg['Content'][3:])
                     # 发送消息
                     itchat.send_msg(sendMsg, user['UserName'])
-            print('所有用户数量: ' + len(users))
+                else:
+                    not_send.append(userRemarkName)
+            print('所有用户数量: ' + str(len(users)))
         elif '发送群' in msg['Content'][0:3]:
             # 要发送的消息
             sendMsg = text_br_index(msg['Content'][3:])
@@ -45,13 +50,14 @@ def new_year(msg):
                     # 对群内发送消息
                     toUserName = group['UserName']
                     itchat.send_msg(sendMsg, toUserName)
-            print('所有群数量: ' + len(groups))
+            print('所有群数量: ' + str(len(groups)))
         elif '非好友' == msg['Content']:
             # 获取非好友对象
             print(not_friends())
         elif '清空' == msg['Content']:
             # 获取非好友对象
             print(clean_not_friends())
+        itchat.send_msg('未发送\n' + str(not_send), groupUserName)
     else:
         # 如果是接收到好友发送的消息, 则进行备份, 用户
         backup(msg)
